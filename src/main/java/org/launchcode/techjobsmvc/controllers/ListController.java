313fc07a01fb72419c16fbe15fc3ffc0137767de
Sarah Dao <sarahdao@Sarahs-MacBook-Pro-3.local>
2023-10-11 15:43:16 -0500

    Added view all link and table for jobs

--- src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
@@ -49,7 +49,7 @@ public class ListController {
     @GetMapping(value = "jobs")
     public String listJobsByColumnAndValue(Model model, @RequestParam String column, @RequestParam(required = false) String value) {
         ArrayList<Job> jobs;
-        if (column.equals("all")){
+        if (column.equalsIgnoreCase("all")){
             jobs = JobData.findAll();
             model.addAttribute("title", "All Jobs");
         } else {
